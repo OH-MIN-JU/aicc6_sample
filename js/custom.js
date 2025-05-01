fe5e9e922f9a6e9bcb27eca1b5fec895c4088458
OH-MIN-JU <rjatnsdl5755@naver.com>
2025-05-01 17:53:50 +0900

    0501

---
+++ js/custom.js
@@ -0,0 +1,21 @@
+// menu toggle
+
+const menu = document.querySelector(".menu");
+const icon = document.querySelector(".menu i");
+
+// menu를 클릭하면 active라는 클래스가 생성됐다 안 됐다 할 수 있음.
+menu.addEventListener("click", function(){
+    this.classList.toggle("active");
+
+    // if(this.classList.contains('active')) {
+    //     // console.log("있다");
+    //     icon.setAttribute("class", "ri-close-line");
+    // } else {
+    //     // console.log("없다");
+    //     icon.setAttribute("class", "ri-menu-line");
+    // }
+
+    (this.classList.contains('active')) ? icon.setAttribute("class", "ri-close-line") : icon.setAttribute("class", "ri-menu-line");
+})
+
+console.log(menu);
